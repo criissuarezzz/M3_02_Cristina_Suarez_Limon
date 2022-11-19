e15fe4c5e12f3b21cf7f3febd4f6233dd8eddde1
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2022-11-19 13:12:53 +0100

    commit

---
+++ ejercicio1.py
@@ -0,0 +1,14 @@
+from ast import main
+
+def division(numerador, denominador):
+    try:
+        numero=numerador/denominador
+        return numero
+    except ZeroDivisionError:
+        return("Error. No es posible la división de un número entre 0.")
+
+def main():         #Creamos la función main
+    print(division(7,0))
+
+if __name__=="main__":
+    main()
